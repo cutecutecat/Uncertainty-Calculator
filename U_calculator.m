d5baf81d5fe6002f5f3c5e69dfe20888388ed68a
Junyu Chen <kind@mail.ustc.edu.cn>
2017-06-16 17:29:18 +0800

    Add files via upload

---
+++ U_calculator.m
@@ -0,0 +1,33 @@
+clear ; close all; clc;
+A=input('输入数据：格式[1,2,3,4,...]\n');
+percentage_A=[...
+    1.32, 1.20, 1.14, 1.11, 1.09, 1.08, 1.07, 1.06, 1.00;...
+    2.92, 2.35, 2.13, 2.02, 1.94, 1.86, 1.83, 1.76, 1.65;...
+    4.30, 3.18, 2.78, 2.57, 2.46, 2.37, 2.31, 2.26, 1.96;...
+    9.93, 5.84, 4.60, 4.03, 3.71, 3.50, 3.36, 3.25, 2.58];
+percentage_B=[...
+    1.000,1.183;...
+    1.650,1.559;...
+    1.960,1.645;...
+    2.580,1.715;];
+percentage_B(:,1)=percentage_B(:,1)/3;
+percentage_B(:,2)=percentage_B(:,2)/(3^0.5);
+A_mean=mean(A,2);
+Ua=std(A,0,2)/sqrt(length(A));
+Da=Ua*percentage_A(:,min(length(A)-2,9));
+tool_d=input('输入仪器误差或0\n');
+cal_d=input('机械仪表输入最小分度，数字仪表输入0\n');
+Ub=(tool_d^2+(cal_d/2)^2)^0.5;
+B_type=input('正态分布输入1，均匀分布输入2\n');
+Db=Ub*percentage_B(:,B_type);
+Up=power((power(Da,2)+power(Db,2)),0.5);
+Up(:,4)=Up;
+Up(:,1)=[0.68;0.90;0.95;0.99];Up(:,2)=Da;Up(:,3)=Db;
+clc;
+fprintf('数据');
+disp(A);
+fprintf('平均值  %f\n',A_mean);
+fprintf('平均值的标准差UA  %f\n',Ua);
+fprintf('B类不确定度最大值UB  %f\n',Ub);
+fprintf('   可信概率     A类       B类    合成不确定度\n');
+disp(Up)
